86b53f92ee554eeeb95f5f3a364476ee388b9ccb
ssmorales <ssmorales@mail.csuchico.edu>
2018-09-05 17:31:05 -0700

    Put my name as the author and changed the text of the code itself.

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,12 @@
+// Author: Samual Morales
 #include <iostream>
 #include <string>
 
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hello my name is Samual Morales." << std::endl;
+    std::cout << "What is yours?" << std::endl;
     std::cin >> input;
-    std::cout << "Hello, " << input << std::endl;
+    std::cout << "Nice to meet you " << input << "! It will be good working with you." << std::endl;
     return 0;
 }
